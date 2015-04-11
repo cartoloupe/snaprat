2168d860575d6975cb5d27dfb30ece6370c95568
Vasanth Pappu <vpappu@covermymeds.com>
2015-04-10 22:41:11 -0400

    refactor into SnapRat class

--- spec/doc_spec.rb
+++ spec/doc_spec.rb
@@ -1,9 +1,43 @@
 require 'rspec'
+require 'pry'
+require_relative '../snaprat.rb'
 
-RSpec.describe 'snaprat' do
+RSpec.describe SnapRat do
+  let(:file) {'files/diceroni.png'}
+  let(:time) { File.ctime(file)}
+  let(:directory) {'tmp'}
 
-  it 'sorts files' do
-    expect(1).to eq 1
+  before :all do
+    `rm -rf tmp` if Dir.exists? 'tmp'
   end
+
+  it 'knows the creation dates of files' do
+    expect(SnapRat.gettime(file).to_s).to match '2015-04-10 21:41:17 -0400'
+  end
+
+  it 'splits timestamp of files into directory structures' do
+
+    expect(SnapRat.timestamp_to_directories(time)).to eq [2015, 4, 10]
+  end
+
+  it 'can check the contents of a directory' do
+    `mkdir #{directory}`
+
+    expect(SnapRat.contents directory).to eq []
+
+    # create a file in the dir
+    `touch #{directory}/tmpfile`
+    expect(SnapRat.contents directory).to eq ['tmp/tmpfile']
+
+    # cleanup
+    `rm #{directory}/*`
+    expect(SnapRat.contents directory).to eq []
+
+  end
+
+  xit 'sorts files into directory structure of year/month/day' do
+    expect(SnapRat.target_directory(file)).to eq '2015/04/10'
+  end
+
 
 end
